cd8f9baa17c5e113d294b293038817bfa9486692
(David) Siu-Kei Muk <muksiukei@gmail.com>
2019-05-13 13:49:43 +0800

    Adding docstring to "Pipeline"

--- tinypipe/pipeline.py
+++ tinypipe/pipeline.py
@@ -25,6 +25,81 @@ from tinypipe.blocks import pipe as pipe_lib
 
 
 class Pipeline(object):
+  """`Pipeline` groups a series of pipes into an object with
+
+  Usage of `Pipeline` involves the following stages:
+    1. Construct `Pipeline` by defining the responsible `Pipe` and append them
+       to the pipeline in order.
+    2. Build the `Pipeline` with `pipeline.build()`.
+    3. Start the `Pipeline` with `pipeline.start()`.
+    4. Feed in each data to be processed by `pipeline.put(data)`.
+    5. Signal `Pipeline` to wrap up with `pipeline.join()`
+       when no more data is to be fed to it.
+
+  The order of the operation units follows the order of `Pipe`s appended to the
+  pipeline. Each `Pipe` owns a thread and runs in parallel. Queues are created
+  to chain the `Pipe`s during the `build()` call, and only after then data may
+  be fed to the pipeline. The `start()` method signals all the `Pipe`s to start
+  operating. Data can be fed to the pipeline through `pipeline.put(data)`. Once
+  `pipeline.join()` is called, the pipeline will no longer accept further data,
+  and signals the underlying `Pipe`s to wrap up and shutdown once they have
+  finished processing all the pending data, in series.
+
+  Here is an example:
+
+  The pipeline below does the following:
+    1. Batch the numbers
+    2. Add one to each number
+    3. Form a pair (2*n-1, -n) for each number
+    4. Print each pair
+
+  ```python
+  import tinypipe as tp
+
+  def f1(nums):
+    return [n + 1 for n in nums]
+
+  def f2(nums):
+    return [(2 * n - 1, -n) for n in nums]
+
+  def f3(pairs):
+    for pair in pairs:
+      print("(%s, %s)" % pair)
+    return pairs
+
+  batch_pipe = tp.pipe.BatchPipe(100)
+  func_pipe_1 = tp.pipe.FunctionPipe(f1)
+  func_pipe_2 = tp.pipe.FunctionPipe(f2)
+  func_pipe_3 = tp.pipe.FunctionPipe(f3)
+
+  pipeline.append(batch_pipe)
+  pipeline.append(func_pipe_1)
+  pipeline.append(func_pipe_2)
+  pipeline.append(func_pipe_3)
+  # NOTE: One can call `pipeline.extend(pipes)` instead
+  ```
+
+  Build and start the pipeline:
+
+  ```python
+  pipeline.build()
+  pipeline.start()
+  ```
+
+  Feed data into the pipeline:
+
+  ```python
+  for i in range(2047):
+    pipeline.put(i)
+  ```
+
+  When all data is fed into the pipeline, signal the pipeline to wrap up and
+  stop once all data is processed.
+
+  ```python
+  pipeline.join()
+  ```
+  """
   def __init__(self, capacity=None, cooldown_secs=None):
     self._capacity = capacity
     self._cooldown_secs = cooldown_secs
